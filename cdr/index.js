8b17f48bddb027d0eeda5075d498cbba26a32b0d
kb <kbibireddy@windhoverlabs.com>
2018-12-02 17:48:00 -0600

    added proper names

--- cdr/index.js
+++ cdr/index.js
@@ -11,7 +11,7 @@ module.exports = class CfeCdrPlugin extends CdrPlugin {
 
   getContent() {
     var result = {
-      shortDescription: 'SENS',
+      shortDescription: 'Sensor Combined',
       longDescription: 'Sensor Combined Message.',
       nodes: {
         main: {
